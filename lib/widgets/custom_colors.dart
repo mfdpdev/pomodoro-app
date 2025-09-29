4c4fd3bcabba4c8b7a4fb6c15fc56d5bb002752b
lord <danipramoedya2002@gmail.com>
2025-09-29 16:15:13 +0700

    doc: Remove some sh1t there and remove build folder from git tracking

--- lib/widgets/custom_colors.dart
+++ lib/widgets/custom_colors.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 
 class CustomColors {
-  // static const Color primary = Color(0xFF1ABC9C);
-  // static const Color secondary = Color(0xFF2C3E50);
-  // static const Color accent = Color(0xFFE74C3C);
   static const Color background = Color(0xFFFFFFFF);
   static const Color text = Color(0xFF333333);
 
